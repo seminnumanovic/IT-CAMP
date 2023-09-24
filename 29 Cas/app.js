add27a3f265754b62c37cc4287c9537c71681c5a
seminnumanovic <semin.contact@gmail.com>
2023-09-24 17:49:16 +0200

    initial commit

---
+++ 29 Cas/app.js
@@ -0,0 +1,135 @@
+// 5.3
+// let n = parseInt(prompt("Unesite n: "))
+
+// let i = 1;
+// var suma = 0;
+// while( i <= n)
+
+// {
+//     suma += i;
+//     i++;
+// }
+// console.log(suma);
+
+
+// 5.4
+
+// let n = parseInt(prompt("Unesite n: "))
+
+// let i = 1;
+// var suma = 0;
+
+// while( i <= n)
+
+// {
+//     suma = suma + i;
+//     i = i+3;
+// }
+// console.log( suma );
+
+// 5.5
+
+// let n = parseFloat(prompt("Unesite n: "))
+
+// let i = 1, s = 0;
+
+// while(i<=n)
+// {
+//     s += 1 / Math.pow(2 * i + 1, 2);
+//     i++;
+// }
+
+// console.log(s);
+
+// let n = parseFloat(prompt("Unesite n: "))
+// let m = parseFloat(prompt("Unesite m: "))
+
+// let i = n, s = 0;
+
+// while ( i <= m){
+//     if ( i % 2 == 0)
+//     {
+//         s = s + Math.pow ( i , 2);
+//     }
+//     else s = s + Math.pow ( i , 3)
+//     i++;
+// }
+// console.log(s);
+
+// let n = parseInt(prompt("Unesite broj elemenata:"))
+// let fp = 1, fpp = 1, i = 3, su = 2 , pom; 
+// let rowOutput = '1 1 ';
+// let 
+
+// while( i <= n){
+//     pom = fp + fpp;
+//     s += pom;
+//     rowOutput += `${c}`
+//     fp = fpp;
+//     fpp = pom;
+//     i++;
+// }
+// console.log(rowOutput, s);
+
+//////////// 5.8
+
+// let n = parseInt(prompt("Unesite n:"))
+// let a = parseInt(prompt("Unesite a:"))
+// let s = 1;
+// let i = 1;
+// while ( i <= n ){
+//     s *= a;
+//     i++  
+// }
+// console.log(s);
+
+// s = Math.pow ( a , n)
+// console.log(s);
+
+////////////////5.9
+
+
+// let n = parseInt(prompt("Unesite n:"))
+// let suma
+// let i = 1, x;
+
+// while ( i != 0)
+// {
+//     suma += n;
+//     i++;
+//     x += parseInt(prompt("Unesi n opet:"))
+// }
+// if ( n === 0 )
+// return ("Nije realan broj")
+// else ("Aritmeticka sredina niza je :" suma)
+
+
+// let suma = 0; iterator = 0;
+// let stop = parseFloat(prompt("Unesite broj:"))
+
+// while( stop !== 0){
+
+//     if(stop >= 2 && stop <= 6)
+//     suma += stop;
+//     iterator ++;
+
+//     stop = parseFloat(prompt("Unesite jos jeadn broj:"))
+
+// }
+// console.log(suma / iterator);
+
+
+// let n = parseFloat(prompt("Unesite n:"))
+// let m = parseFloat(prompt("Unesite m:"))
+// while( n < 1 && m < 1){
+
+// }
+// let p;
+// if( n > 1 && m > 1){
+//     if( n < m){
+//         p = n;
+//         n = m;
+//         m = p;
+//     }
+
+// }
